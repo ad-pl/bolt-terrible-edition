47cc0bbf916e1c8448c323c196daee3896ccf215
_Spark <spark-aur@proton.me>
2025-08-19 13:24:54 +0300

    move all the old moderation cog code to different files (will be cleaned up later)

---
+++ bot/cogs/moderation/old_ban.py
@@ -0,0 +1,100 @@
+# bot/cogs/moderation/ban.py
+
+# LIBRARIES AND MODULES
+
+## pycord
+
+import discord
+from discord.ext import commands
+
+## pypkg
+
+from bot.constants import *
+import bot.console as console
+import bot.utils as utils
+
+# CLASSES
+
+class Ban(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+  async def _ban(self, ctx, member, reason=None, is_slash=False):
+    user = ctx.author
+
+    if not await utils.assert_guild(ctx, guild=ctx.guild, user=user, is_slash=is_slash):
+      return
+
+    try:
+      if user == member:
+        console.log(f"{user} was an idiot and tried to ban themselves.", "LOG")
+        await utils.say(ctx, "You can't ban yourself!", is_slash=is_slash, ephemeral=True)
+        return
+      
+      console.log(f"{user} banned {member}{(' for ' + reason) if reason else '.'}", "LOG")
+      await member.ban(reason=reason or 'None provided.')
+
+    except discord.Forbidden:
+      console.log(f"Failed to ban {member}, permission denied.", "ERROR")
+      await utils.say(ctx, "I don't have permission to ban that user.", is_slash=is_slash, ephemeral=True)
+
+    except Exception as e:
+      console.log(f"Exception raised: {e}", "ERROR")
+      await utils.say(ctx, "Something went wrong, try again later.", is_slash=is_slash, ephemeral=True)
+
+    message = f"Banned {member.mention}. \nReason: {reason or 'None provided.'}"
+
+    await utils.say(ctx, message, is_slash=is_slash)
+
+  async def _unban(self, ctx, member, reason=None, is_slash=False):
+    user = ctx.author
+
+    if not await utils.assert_guild(ctx, guild=ctx.guild, user=user, is_slash=is_slash):
+      return
+
+    try:
+      if user == member:
+        console.log(f"{user} was an idiot and tried to unban themselves.")
+        await utils.say(ctx, "You can't unban yourself!", is_slash=is_slash, ephemeral=True)
+        return
+      
+      console.log(f"{user} unbanned {member}{(' for ' + reason) if reason else ''}.", "LOG")
+      await ctx.guild.unban(member, reason=reason or 'None provided.')
+    
+    except discord.Forbidden:
+      console.log(f"Failed to unban {member}, permission denied.", "ERROR")
+      await utils.say(ctx, "I don't have permission to unban that user.", is_slash=is_slash, ephemeral=True)
+      return
+    
+    except Exception as e:
+      console.log(f"Exception raised: {e}", "ERROR")
+      await utils.say(ctx, "Something went wrong, try again later.", is_slash=is_slash, ephemeral=True)
+      return
+
+    message = f"Unbanned {member.mention}. \nReason: {reason or 'None provided.'}"
+    await utils.say(ctx, message, is_slash=is_slash)
+
+  @commands.command()
+  @commands.has_permissions(ban_members=True)
+  async def ban(self, ctx: commands.Context, member: discord.Member, *, reason: str = "None provided."):
+    await self._ban(ctx, member, reason)
+  
+  @commands.command()
+  @commands.has_permissions(ban_members=True)
+  async def unban(self, ctx: commands.Context, member: discord.User, *, reason: str = "None provided."):
+    await self._unban(ctx, member, reason)
+
+  @commands.slash_command(name="ban", description="ban a user")
+  @commands.has_permissions(ban_members=True)
+  async def slash_ban(self, ctx: discord.ApplicationContext, member: discord.Member, reason: str = "None provided."):
+    await self._ban(ctx, member, reason, is_slash=True)
+  
+  @commands.slash_command(name="unban", description="unban a previously banned user")
+  @commands.has_permissions(ban_members=True)
+  async def slash_unban(self, ctx: discord.ApplicationContext, member: discord.User, reason: str = "None provided."):
+    await self._unban(ctx, member, reason, is_slash=True)
+
+# FUNCTIONS
+
+def setup(bot):
+  bot.add_cog(Ban(bot))
---
+++ bot/cogs/moderation/old_timeout.py
@@ -0,0 +1,102 @@
+# bot/cogs/moderation/timeout.py
+
+# LIBRARIES AND MODULES
+
+from datetime import timedelta
+
+## pycord
+
+import discord
+from discord.ext import commands
+
+## pypkg
+
+import bot.console as console
+import bot.utils as utils
+
+# CLASSES
+
+class Timeout(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+  async def _mute(self, ctx, member: discord.Member, duration="30m", reason=None, is_slash=False):
+    user = ctx.author
+
+    if not await utils.assert_guild(ctx, guild=ctx.guild, user=user, is_slash=is_slash):
+      return
+
+    seconds = utils.parse_duration(duration)
+
+    if seconds is None:
+      await utils.say(ctx, "You forgot to specify a duration. The default is 30 minutes.")
+      # no early return, just a warning.
+
+    if not seconds:
+      await utils.say(ctx, "Invalid duration format. Try `3d`, `1h`, `30m`, `45s`", is_slash=is_slash, ephemeral=True)
+      return
+    
+    try:
+      if user == member:
+        console.log(f"{user} was an idiot and tried to mute themselves.")
+        await utils.say(ctx, "You can't mute yourself!", is_slash=is_slash, ephemeral=True)
+        return
+      
+      await member.timeout_for(timedelta(seconds=seconds), reason=reason or 'None provided.')
+      console.log(f"{user} muted {member} for {duration}. Reason: {reason or 'None provided'}", "LOG")
+
+      await utils.say(ctx, f"Muted {member.mention} for {duration}. \nReason: {reason or 'None provided.'}", is_slash=is_slash)
+
+    except discord.Forbidden:
+      await utils.say(ctx, "I don't have permission to mute that user.", is_slash=is_slash, ephemeral=True)
+
+    except Exception as e:
+      console.log(f"Exception raised: {e}", "ERROR")
+      await utils.say(ctx, "Something went wrong, try again later.", is_slash=is_slash, ephemeral=True)
+  
+  async def _unmute(self, ctx, member: discord.Member, is_slash=False):
+    user = ctx.author
+
+    if not await utils.assert_guild(ctx, guild=ctx.guild, user=user, is_slash=is_slash):
+      return
+
+    try:
+      if user == member:
+        console.log(f"{user} was an idiot and tried to unmute themself despite being unmuted anyway", "LOG")
+        await utils.say(ctx, "You're already unmuted.", is_slash=is_slash, ephemeral=True)
+        return
+      
+      await member.remove_timeout(reason="you're not getting a reason my friend")
+      console.log(f"{user} unmuted {member}.", "LOG")
+      await utils.say(ctx, f"Unmuted {member.mention}.", is_slash=is_slash)
+    except discord.Forbidden:
+      console.log(f"Failed to unmute {member}, permission denied.", "LOG")
+      await utils.say(ctx, "I don't have permission to unmute that user.", is_slash=is_slash, ephemeral=True)
+    except Exception as e:
+      console.log(f"Exception raised: {e}", "ERROR")
+      await utils.say(ctx, "Something went wrong, try again later.", is_slash=is_slash, ephemeral=True)
+
+  @commands.command()
+  @commands.has_permissions(moderate_members=True)
+  async def mute(self, ctx: commands.Context, member: discord.Member, duration="30m", *, reason=None):
+    await self._mute(ctx, member, duration, reason)
+
+  @commands.command()
+  @commands.has_permissions(moderate_members=True)
+  async def unmute(self, ctx: commands.Context, member: discord.Member):
+    await self._unmute(ctx, member)
+
+  @commands.slash_command(name="mute", description="[moderation] mute a member.")
+  @commands.has_permissions(moderate_members=True)
+  async def slash_mute(self, ctx: discord.ApplicationContext, member: discord.Member, duration="30m", reason=None):
+    await self._mute(ctx, member, duration, reason, is_slash=True)  
+
+  @commands.slash_command(name="unmute", description="unmute a member")
+  @commands.has_permissions(moderate_members=True)
+  async def slash_unmute(self, ctx: discord.ApplicationContext, member: discord.Member):
+    await self._unmute(ctx, member, is_slash=True)
+
+# FUNCTIONS
+
+def setup(bot):
+  bot.add_cog(Timeout(bot))
