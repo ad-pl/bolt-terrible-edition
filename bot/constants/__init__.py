a6c410c0ae467a4536fc8a42824f8533d11fe211
_Spark <spark-aur@proton.me>
2025-09-05 09:34:53 +0300

    feat: add * imports inside the constants file to make it easier to work with

--- bot/constants/__init__.py
+++ bot/constants/__init__.py
@@ -1,2 +1,8 @@
+#!/usr/bin/env python3
 # okay dude this is getting ridiculous 
 # why is there like 5 __init__.py files in this whole repo
+
+from .base import *
+from .colors import *
+from .config import *
+from .moderation import *
