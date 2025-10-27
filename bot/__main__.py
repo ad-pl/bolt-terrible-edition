735fd38643eb657fc6fa6e1c7b1fa0fa960753e3
_Spark <spark-aur@proton.me>
2025-10-27 19:13:56 +0300

    refactor: remove the directory safeguard because its useless
    resolves issue #25

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,30 +2,8 @@
 
 # bot/__main__.py
 '''
-main entry point
-
-recommended to use this file instead of running bot.start_bot() directly
-NOTE: the above line is not true.
+main entry point.
 '''
-
-# SAFEGUARDS
-
-def dir_safeguard():
-  '''
-  simple check to see if you're running the bot from the repo root.
-  NOTE: because the safeguard only runs in this file, people can easily just run `bot.start_bot()` directly
-        and see the effects of running bolt outside the root. (spoiler: it will crash.)
-  
-  NOTE: yeah no this is useless. this safeguard is pointless because of how python PATHs work, will remove later.
-  '''
-
-  from pathlib import Path
-
-  cwd = Path.cwd()
-  root = Path(__file__).resolve().parent.parent # in other words, the parent directory of the directory that __file__ is in
-
-  if cwd != root:
-    raise RuntimeError(f"Never run Bolt from outside this directory: {root}\nYou're in the {cwd} directory.")
 
 # LIBRARIES AND MODULES
 
@@ -41,8 +19,6 @@ def main():
   '''
   le bootstrapper
   '''
-
-  dir_safeguard()
   
   try:
     console.log("Starting Bolt...", "LOG")
